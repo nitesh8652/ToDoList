47b4a0dd7ea3459e214d0f6c5cbb8c56b3e52563
nitesh8652 <niteshsalian8652085233>
2024-12-15 20:41:17 +0530

    ooh

---
+++ script.js
@@ -0,0 +1,6 @@
+// console.log("hello");
+// const done = document.querySelector(".selected");
+// done.addEventListener("click",()=>{
+//     done.style.color="red"
+//     done.src="images/checked.png"
+// })
