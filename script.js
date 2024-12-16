108042106787543ddc896155073116272561436b
nitesh8652 <niteshsalian8652085233>
2024-12-16 21:03:25 +0530

    functionality

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// console.log("hello");
-// const done = document.querySelector(".selected");
-// done.addEventListener("click",()=>{
-//     done.style.color="red"
-//     done.src="images/checked.png"
-// })
+console.log("Hey There!");
